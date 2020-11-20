0007f695a3c322da5df1e9b33fb26a5e95d3e0ba
hexsorcerer <hexsorcerer03e9@gmail.com>
2020-11-20 13:11:01 -0500

    Add project files
    Added existing project files, in relation to netmq-monitor-client project. This
    is the server side of that.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # netmq-monitor-server
 A sample application to try and understand how the NetMQ monitor type works
+
+This was originall a throwaway application to try and figure out how the NetMQ
+monitor type works (see the related netmq-monitor-client project for more
+details). Putting this up so someone else can help me take a look at it.
+
+I may clean this up into a proper example if we get it figured out.
---
+++ src/NetMq.Monitor.Server/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using NetMQ;
+using NetMQ.Monitoring;
+using NetMQ.Sockets;
+
+namespace RouterSocketTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var routerSocket = new RouterSocket();
+            var endpoint = "tcp://0.0.0.0:49152";
+            var monitor = new NetMQMonitor(routerSocket, endpoint);
+
+            routerSocket.Bind(endpoint);
+            Console.WriteLine($"Router socket bound to {endpoint}");
+
+            NetMQMessage message = new NetMQMessage();
+            while (true)
+            {
+                PrintMenu();
+                Console.WriteLine("Enter your selection: ");
+                var selection = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                switch (selection)
+                {
+                    case 1:
+                        SendMessage(routerSocket);
+                        break;
+                    case 2:
+                        message = WaitForMessage(routerSocket);
+                        var itr = message.GetEnumerator();
+                        Console.WriteLine(
+                            string.Format("{0} greeting frame", itr.MoveNext() ? "found" : "not found"));
+                        Console.WriteLine(
+                            string.Format("Message: {0}", itr.Current.ConvertToString()));
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            };
+        }
+
+        private static void PrintMenu()
+        {
+            Console.WriteLine("Select an option:");
+            Console.WriteLine();
+            Console.WriteLine("1: Send a message");
+            Console.WriteLine("2: Wait for a message");
+            Console.WriteLine();
+        }
+
+        private static void SendMessage(RouterSocket socket)
+        {
+            var message = new NetMQMessage();
+            Console.WriteLine("Type the message you want to send: ");
+            var body = Console.ReadLine();
+            message.Append(body);
+
+            socket.SendMultipartMessage(message);
+            Console.WriteLine();
+            Console.WriteLine("Sent message to router socket");
+            Console.WriteLine();
+        }
+
+        private static NetMQMessage WaitForMessage(RouterSocket socket)
+        {
+            Console.WriteLine("Waiting on message from router socket");
+            return socket.ReceiveMultipartMessage();
+        }
+
+        private void OnDisconnected(NetMQMonitorErrorEventArgs e)
+        {
+            Console.WriteLine("Disconnected");
+        }
+    }
+}
